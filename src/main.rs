90735f0d705a8a6376e2a2e357184c8cc57023cd
0xtriboulet <santos.s.salinas@gmail.com>
2024-04-27 18:54:26 -0400

    Refactor main functionality and optimize release profile
    In the Cargo.toml file, new rules are set to optimize the performance of a release build. The 'main' function and 'capture_image' have been significantly revised for a simpler, more error-explanatory implementation. The main functionality of checking the similarity between images has been encapsulated into a separate 'check_similarity' function, enhancing code readability.

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,15 @@ use image_crate::{ImageBuffer, ImageResult, Rgb};
 
 // LIBTORCH_BYPASS_VERSION_CHECK= 1
 
-
 fn capture_image() -> ImageResult<()> {
 
     // request the absolute highest resolution CameraFormat that can be decoded to RGB.
     let requested = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
-    // make the camera
-    let mut camera = unsafe { Camera::new(CameraIndex::Index(0), requested).unwrap() };
-
-    // get a frame
-    let frame = camera.frame().unwrap();
+   
+    // make the get frame from camera
+    let mut frame = Camera::new(CameraIndex::Index(0), requested)
+        .expect("Failed to initialize camera!")
+        .frame().expect("Failed to grab frame!");;
 
     // decode into an ImageBuffer
     let decoded = frame.decode_image::<RgbFormat>().unwrap();
@@ -34,28 +33,6 @@ fn capture_image() -> ImageResult<()> {
     img_buffer.expect("Failed to read image into buffer!").save("captured_frame.png")
 }
 
-// TEST
-fn main() -> anyhow::Result<()> {
-    
-    capture_image().expect("Failed to capture image!");
-
-    let model = CModule::load("model.pt")?;
-
-    let img1_path = "reference_frame.png";
-    let img2_path = "captured_frame.png";
-
-    let img1 = preprocess_image(img1_path)?;
-    let img2 = preprocess_image(img2_path)?;
-
-    let embedding1 = model.forward_ts(&[img1.to_kind(Kind::Float)])?;
-    let embedding2 = model.forward_ts(&[img2.to_kind(Kind::Float)])?;
-
-    let similarity = Tensor::cosine_similarity(&embedding1, &embedding2, 1, 1e-6);
-    println!("Similarity score: {:?}", similarity.double_value(&[]));
-
-    Ok(())
-}
-
 fn preprocess_image(img_path: &str) -> anyhow::Result<Tensor> {
     let image = image::load(img_path)?.resize([3,160,160]).to_kind(Kind::Uint8) / 256.0; // Normalize image to 0-1
 
@@ -65,3 +42,36 @@ fn preprocess_image(img_path: &str) -> anyhow::Result<Tensor> {
 
     Ok(image.unsqueeze(0).to_kind(Kind::Float)) // Add batch dimension
 }
+
+
+fn check_similarity()-> f64{
+
+    capture_image().expect("Failed to capture image!");
+
+    let model = CModule::load("model.pt").expect("Failed to load model!");
+
+    let img1_path = "reference_frame.png";
+    let img2_path = "captured_frame.png";
+
+    let img1 = preprocess_image(img1_path).expect("Failed to preprocess img1!");
+    let img2 = preprocess_image(img2_path).expect("Failed to preprocess img2!");
+
+    let embedding1 = model.forward_ts(&[img1.to_kind(Kind::Float)]).expect("Failed to extract embedding1!");
+    let embedding2 = model.forward_ts(&[img2.to_kind(Kind::Float)]).expect("Failed to extract embedding2!");
+
+    f64::try_from(Tensor::cosine_similarity(&embedding1, &embedding2, 1, 1e-6)).unwrap()
+
+}
+
+// TEST
+fn main() -> anyhow::Result<()> {
+
+    capture_image().expect("Failed to capture image!");
+
+    let similarity = check_similarity();
+
+    println!("Similarity score: {:?}", similarity);
+
+    Ok(())
+}
+
